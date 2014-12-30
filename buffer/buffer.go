16d767311f305dae9642a88ce6bef45fafa4c0cf
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2014-12-30 22:18:18 +0100

    Add Profiles endpoints

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -1,72 +1,74 @@
 package buffer
 
 import (
-    "encoding/json"
-    "net/http"
-    "net/url"
+	"encoding/json"
+	"net/http"
+	"net/url"
 )
 
 const (
-    ApiURL = "https://api.bufferapp.com"
+	ApiURL = "https://api.bufferapp.com"
 )
 
 type Client struct {
-    AccessToken  string
-    BaseURL *url.URL
-    client  *http.Client
+	AccessToken string
+	BaseURL     *url.URL
+	client      *http.Client
 
-    User *UserService
+	Profiles *ProfilesService
+	User     *UserService
 }
 
 func NewClient(accessToken string, httpClient *http.Client) *Client {
-    if httpClient == nil {
-        httpClient = http.DefaultClient
-    }
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
 
-    baseURL, _ := url.Parse(ApiURL)
+	baseURL, _ := url.Parse(ApiURL)
 
-    client := &Client{
-        AccessToken: accessToken,
-        BaseURL: baseURL,
-        client:  httpClient,
-    }
+	client := &Client{
+		AccessToken: accessToken,
+		BaseURL:     baseURL,
+		client:      httpClient,
+	}
 
-    client.User = &UserService{client: client}
+	client.Profiles = &ProfilesService{client: client}
+	client.User = &UserService{client: client}
 
-    return client
+	return client
 }
 
 func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
-    resp, err := c.client.Do(req)
-    if err != nil {
-        return nil, err
-    }
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    if v != nil {
-        err = json.NewDecoder(resp.Body).Decode(v)
-    }
+	if v != nil {
+		err = json.NewDecoder(resp.Body).Decode(v)
+	}
 
-    return resp, err
+	return resp, err
 }
 
 func (c *Client) NewRequest(method, urlStr string, params interface{}) (*http.Request, error) {
-    rel, err := url.Parse(urlStr)
-    if err != nil {
-        return nil, err
-    }
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
 
-    u := c.BaseURL.ResolveReference(rel)
-    q := u.Query()
-    u.RawQuery = q.Encode()
+	u := c.BaseURL.ResolveReference(rel)
+	q := u.Query()
+	u.RawQuery = q.Encode()
 
-    req, err := http.NewRequest(method, u.String(), nil)
-    if err != nil {
-        return nil, err
-    }
+	req, err := http.NewRequest(method, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
 
-    req.Header.Add("Authorization", "Bearer " + c.AccessToken)
+	req.Header.Add("Authorization", "Bearer "+c.AccessToken)
 
-    return req, nil
+	return req, nil
 }
---
+++ buffer/profiles.go
@@ -0,0 +1,76 @@
+package buffer
+
+import (
+	"fmt"
+)
+
+type ProfilesService struct {
+	client *Client
+}
+
+type Profiles struct {
+	Items []Profile
+}
+
+type Profile struct {
+	Avatar            string                 `json:"avatar,omitempty"`
+	CreatedAt         int                    `json:"created_at,omitempty"`
+	Default           bool                   `json:"default,omitempty"`
+	FormattedUsername string                 `json:"formatted_username,omitempty"`
+	ID                string                 `json:"id,omitempty"`
+	Schedules         []ProfileSchedule      `json:"schedules,omitempty"`
+	Service           string                 `json:"service,omitempty"`
+	ServiceID         string                 `json:"service_id,omitempty"`
+	ServiceUsername   string                 `json:"service_username,omitempty"`
+	Statistics        map[string]interface{} `json:"statistics,omitempty"`
+	TeamMembers       []string               `json:"team_members,omitempty"`
+	Timezone          string                 `json:"timezone,omitempty"`
+	UserID            string                 `json:"user_id,omitempty"`
+}
+
+type ProfileSchedule struct {
+	Days  []string `json:"days,omitempty"`
+	Times []string `json:"times,omitempty"`
+}
+
+func (s *ProfilesService) Get(profileID string) (*Profile, error) {
+	u := fmt.Sprintf("/1/profiles/%v.json", profileID)
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	profile := new(Profile)
+	_, err = s.client.Do(req, profile)
+
+	return profile, err
+}
+
+func (s *ProfilesService) GetAll() (*Profiles, error) {
+	u := "/1/profiles.json"
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	profiles := new(Profiles)
+	_, err = s.client.Do(req, profiles)
+
+	return profiles, err
+}
+
+func (s *ProfilesService) GetSchedules(profileID string) (*ProfileSchedule, error) {
+	u := fmt.Sprintf("/1/profiles/%v.json", profileID)
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	schedule := new(ProfileSchedule)
+	_, err = s.client.Do(req, schedule)
+
+	return schedule, err
+}
