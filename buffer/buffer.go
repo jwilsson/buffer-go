b8bc91ff8e6ca725397cf6e6bba7bc74cf7a8fb7
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2014-12-30 22:59:55 +0100

    Add query string support

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -1,76 +1,87 @@
 package buffer
 
 import (
-	"encoding/json"
-	"net/http"
-	"net/url"
+    "encoding/json"
+    "net/http"
+    "net/url"
+
+    "github.com/google/go-querystring/query"
 )
 
 const (
-	ApiURL = "https://api.bufferapp.com"
+    ApiURL = "https://api.bufferapp.com"
 )
 
 type Client struct {
-	AccessToken string
-	BaseURL     *url.URL
-	client      *http.Client
+    AccessToken string
+    BaseURL     *url.URL
+    client      *http.Client
 
-	Profiles *ProfilesService
-	Updates  *UpdatesService
-	User     *UserService
+    Profiles *ProfilesService
+    Updates  *UpdatesService
+    User     *UserService
 }
 
 func NewClient(accessToken string, httpClient *http.Client) *Client {
-	if httpClient == nil {
-		httpClient = http.DefaultClient
-	}
+    if httpClient == nil {
+        httpClient = http.DefaultClient
+    }
 
-	baseURL, _ := url.Parse(ApiURL)
+    baseURL, _ := url.Parse(ApiURL)
 
-	client := &Client{
-		AccessToken: accessToken,
-		BaseURL:     baseURL,
-		client:      httpClient,
-	}
+    client := &Client{
+        AccessToken: accessToken,
+        BaseURL:     baseURL,
+        client:      httpClient,
+    }
 
-	client.Profiles = &ProfilesService{client: client}
-	client.Updates = &UpdatesService{client: client}
-	client.User = &UserService{client: client}
+    client.Profiles = &ProfilesService{client: client}
+    client.Updates = &UpdatesService{client: client}
+    client.User = &UserService{client: client}
 
-	return client
+    return client
 }
 
 func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
+    resp, err := c.client.Do(req)
+    if err != nil {
+        return nil, err
+    }
 
-	defer resp.Body.Close()
+    defer resp.Body.Close()
 
-	if v != nil {
-		err = json.NewDecoder(resp.Body).Decode(v)
-	}
+    if v != nil {
+        err = json.NewDecoder(resp.Body).Decode(v)
+    }
 
-	return resp, err
+    return resp, err
 }
 
 func (c *Client) NewRequest(method, urlStr string, params interface{}) (*http.Request, error) {
-	rel, err := url.Parse(urlStr)
-	if err != nil {
-		return nil, err
-	}
+    rel, err := url.Parse(urlStr)
+    if err != nil {
+        return nil, err
+    }
 
-	u := c.BaseURL.ResolveReference(rel)
-	q := u.Query()
-	u.RawQuery = q.Encode()
+    u := c.BaseURL.ResolveReference(rel)
+    q := u.Query()
 
-	req, err := http.NewRequest(method, u.String(), nil)
-	if err != nil {
-		return nil, err
-	}
+    if params != nil {
+        // Merge all query parameters
+        data, _ := query.Values(params)
+        for key, val := range data {
+            q.Add(key, val[0])
+        }
+    }
 
-	req.Header.Add("Authorization", "Bearer "+c.AccessToken)
+    u.RawQuery = q.Encode()
 
-	return req, nil
+    req, err := http.NewRequest(method, u.String(), nil)
+    if err != nil {
+        return nil, err
+    }
+
+    req.Header.Add("Authorization", "Bearer "+c.AccessToken)
+
+    return req, nil
 }
